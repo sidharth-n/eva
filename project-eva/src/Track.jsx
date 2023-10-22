f2505ae40844f24fc9f6383c7cf4f81390d882f7
sidharth-n <sidharthnofficial@gmail.com>
2023-10-22 11:52:02 +0530

    woking with 2 tracks

--- project-eva/src/Track.jsx
+++ project-eva/src/Track.jsx
@@ -1,31 +1,29 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { useGLTF } from "@react-three/drei";
 
 function Track() {
-  const [zPosition, setZPosition] = useState(-20); // Initial Z position
-  const track = useGLTF("./env.glb");
+  const { scene: trackScene } = useGLTF("./env.glb");
+
+  // Clone the original scene for each track
+  const track1 = trackScene.clone();
+  const track2 = trackScene.clone();
 
   useEffect(() => {
-    const intervalId = setInterval(() => {
-      setZPosition((prevZ) => {
-        const newZ = prevZ + 1; // Adjust this value to control the speed
-        if (newZ > 150) {
-          clearInterval(intervalId); // Stop the interval once Z position reaches 150
-        }
-        return newZ;
-      });
-    }, 10); // Adjust this value to control the frequency of updates
+    // Set initial positions, scales, and rotations for both tracks
+    const setupTrack = (track, zPosition) => {
+      track.position.set(0, -4, zPosition);
+      track.scale.set(0.01, 0.01, 0.01);
+      track.rotation.set(0, 1.57, 0);
+    };
 
-    return () => clearInterval(intervalId); // Clear the interval when component unmounts
-  }, []);
-
-  useEffect(() => {
-    track.scene.position.set(0, -4, zPosition);
-  }, [track, zPosition]);
+    setupTrack(track1, -20); // Set the end of the first track at z=0
+    setupTrack(track2, -220); // Set the start of the second track at z=0
+  }, [track1, track2]);
 
   return (
     <>
-      <primitive object={track.scene} receiveShadow />
+      <primitive object={track1} receiveShadow />
+      <primitive object={track2} receiveShadow />
     </>
   );
 }
